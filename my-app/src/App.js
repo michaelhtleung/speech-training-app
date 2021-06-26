7152b33f1c06aa47fa908d64344a05b09e0c74ce
Michael Leung <michaelhtleung@gmail.com>
2021-06-26 01:15:18 -0400

    Add trivial firestore write and read examples

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,8 +3,31 @@ import React from "react";
 import firebase from "firebase";
 
 export default function App() {
-  const firebaseApp = firebase.apps[0];
-  return (
+    const firebaseApp = firebase.apps[0];
+
+    let db = firebaseApp.firestore();
+
+    // // trivial write example
+    // db.collection("users").add({
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815
+    // })
+    // .then((docRef) => {
+    //     console.log("Document written with ID: ", docRef.id);
+    // })
+    // .catch((error) => {
+    //     console.error("Error adding document: ", error);
+    // });
+
+    // trivial read example
+    db.collection("users").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+    });
+
+    return (
       <div>
         <h1>React & Firebase</h1>
         <h2>By @farazamiruddin</h2>
